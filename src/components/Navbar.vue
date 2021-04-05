9374436743a6ad4b394d9be9e443c6ca5fbcae0b
DevDams <ad.aimedesire@gmail.com>
2021-04-05 23:03:14 +0000

    design and add api

---
+++ src/components/Navbar.vue
@@ -0,0 +1,41 @@
+<template>
+  <div id="nav">
+    <div class="navbar">
+      <router-link to="/">Ma météo</router-link>
+      <router-link to="/about">About</router-link>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Navbar'
+}
+</script>
+
+<style scoped>
+#nav {
+  background-color: #242424;
+  height: 70px;
+}
+
+.navbar {
+  width: 85%;
+  height: 100%;
+  margin: auto;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #f2f2f2;
+  text-decoration: none;
+  text-transform: uppercase;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
+}
+</style>
---
+++ src/components/Weather.vue
@@ -0,0 +1,210 @@
+<template>
+  <div class="container">
+    <div class="content">
+      <div class="search_box">
+        <h3>Rechercher une ville :</h3>
+        <input type="text" name="query" v-model="query" @keypress="fetchWeather">
+      </div>
+      <div class="weather" v-if="typeof weather.main != 'undefined'">
+        <div class="city_info">
+          <div class="city_name">
+            <h1>{{ weather.name }}, {{ weather.sys.country }}</h1>
+            <p>{{ dateBuilder() }}</p>
+          </div>
+          <div class="city_coor">
+            <p><span>Longitude :</span> {{ weather.coord.lon }} </p>
+            <p><span>Latitude :</span> {{ weather.coord.lat }}</p>
+          </div>
+          <div class="city_sun">
+            <p class="sunrise"><span>Levé du soleil :</span> 6h01</p>
+            <p class="sunset"><span>Couché du soleil :</span> 17h45</p>
+          </div>
+        </div>
+        <div class="city_weather">
+          <div class="city_weather_temp">
+            <div class="temp">
+              <p class="min">Min <br> 9°C</p>
+              <p class="temp">12°C</p>
+              <p class="max">Max <br> 15°C</p>
+            </div>
+            <p class="mom">{{ weather.weather[0].description }}</p>
+          </div>
+          <div class="weather_info">
+            <div class="wind">
+              <p><span>Vitesse du vent</span> <br> 33km/h</p>
+            </div>
+            <div class="humidity">
+              <p><span>Humidité</span> <br> 10%</p>
+            </div>
+            <div class="precipitation">
+              <p><span>Précipitation</span> <br> 50ml</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      api_key: '4904538d41bfc39b8c10759f5776286c',
+      base_url: 'https://api.openweathermap.org/data/2.5/',
+      query: '',
+      weather: {}
+    }
+  },
+  methods: {
+    fetchWeather (e) {
+      if (e.key == 'Enter') {
+        fetch(`${this.base_url}weather?q=${this.query}&units=metric&lang=fr&APPID=${this.api_key}`)
+         .then(res => {
+           return res.json()
+         })
+         .then(this.setResults)
+      }
+    },
+    setResults (results) {
+      this.weather = results
+      console.log(this.weather.weather)
+    },
+    dateBuilder () {
+      let d = new Date()
+      let months = ["Janvier", "Février", "Mars", "Avril",
+      "Mai", "Juin", "Juillet", "Aout", "Septembre", "Octobre",
+      "Novembre", "Décembre"]
+
+      let days = ["Dimanche", "Lundi", "Mardi", "Mercredi",
+      "Jeudi", "Vendredi", "Samedi"]
+
+      let day = days[d.getDay()]
+      let date = d.getDate()
+      let month = months[d.getMonth()]
+      let year = d.getFullYear()
+
+      return `${day} ${date} ${month} ${year}`
+    }
+  }
+}
+</script>
+
+<style>
+.search_box {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-top: 30px;
+}
+
+.search_box h3 {
+  margin-right: 20px;
+  text-transform: uppercase;
+}
+
+.search_box input {
+  width: 270px;
+  height: 50px;
+  border: 1px solid black;
+  border-radius: 8px;
+  padding: 0 10px;
+}
+
+.search_box input:focus {
+  border: 1px solid black;
+  outline: none;
+}
+
+.weather {
+  display: flex;
+  justify-content: space-around;
+  margin-top: 90px;
+}
+
+.weather .city_info {
+  width: 50%;
+  border-right: 1px solid rgba(0, 0, 0, 0.274);
+}
+
+.weather .city_weather {
+  width: 50%;
+}
+
+.weather .city_info .city_name h1 {
+  font-size: 50px;
+  margin-bottom: 15px;
+  text-transform: capitalize;
+}
+
+.weather .city_info .city_coor {
+  display: flex;
+  justify-content: space-around;
+  margin-top: 40px;
+}
+
+.weather .city_info .city_coor span {
+  font-weight: 600;
+  text-transform: uppercase;
+}
+
+.weather .city_info .city_sun {
+  margin-top: 55px;
+}
+
+.weather .city_info .city_sun .sunrise {
+  margin-bottom: 15px;
+}
+
+.weather .city_info .city_sun span {
+  font-weight: 600;
+  text-transform: uppercase;
+}
+
+
+
+
+.city_weather .city_weather_temp .temp {
+  width: 70%;
+  margin: auto;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  font-size: 65px;
+}
+
+.city_weather .city_weather_temp .temp .max, .min {
+  font-size: 20px;
+}
+
+.city_weather .city_weather_temp .mom {
+  font-size: 25px;
+  text-transform: capitalize;
+}
+
+.city_weather .weather_info {
+  width: 100%;
+  margin: auto;
+  margin-top: 60px;
+  display: flex;
+  justify-content: space-around;
+}
+
+.city_weather .weather_info span {
+  font-size: 21px;
+}
+
+.city_weather .weather_info .wind {
+  width: 33%;
+}
+
+.city_weather .weather_info .humidity {
+  width: 33%;
+  border-left: 1px solid rgba(0, 0, 0, 0.274);
+  border-right: 1px solid rgba(0, 0, 0, 0.274);
+}
+
+.city_weather .weather_info .precipitation {
+  width: 33%;
+}
+</style>
